9b835d7a5e324b9efefd13e7d8412eee1f5e3184
dikac <rigel234@yahoo.com>
2022-08-07 07:42:55 +0700

    ref: improve promp

--- src/prompt.ts
+++ src/prompt.ts
@@ -2,18 +2,48 @@ import Prompts from "prompts";
 import FilterMissing from "./prompt-object/array/filter-missing";
 import Questions from "./questions/questions";
 import FromQuestion from './prompt-object/array/from-question';
+import Union from '../../promise/dist/union';
+import Callable from '../../function/dist/callable';
+import Identity from '../../function/dist/identity';
 
-export default function Prompt<Data extends object>(
-    mapper: Questions<Data>,
-    initial ?: ()=>Partial<Data>|Promise<Partial<Data>>,
-) : Promise<Data> {
+export async function PromptParameters<Type extends object>(
+    question: Questions<Type>,
+    initial : Union<Partial<Type>>,
+    ensure : Callable<[Type], Type> = Identity
+) : Promise<Type> {
 
-    return Promise.resolve(initial ? initial() : {}).then(async (config : Partial<Data>)=>{
+    initial = await initial;
 
-        const missing = FilterMissing(FromQuestion(mapper), config);
+    const missing = FilterMissing(FromQuestion(question), initial);
 
-        const data = await Prompts(missing);
+    const complete = Object.assign(
+        await Prompts(missing),
+        initial
+    ) as Type;
 
-        return Object.assign(data, config) as Data ;
-    });
+    return ensure(complete);
 }
+
+export type PromptArgument<Type extends object> = {
+    question: Questions<Type>,
+    initial?: Union<Partial<Type>>,
+    ensure?: Callable<[Type], Type>
+};
+
+export function PromptParameter<Type extends object>(
+    {
+        question,
+        initial = {},
+        ensure = Identity,
+    } : PromptArgument<Type>
+) {
+    return PromptParameters(question, initial, ensure);
+}
+
+
+namespace Prompt {
+    export const Parameters = PromptParameters;
+    export const Parameter = PromptParameter;
+    export type Argument<Type extends object> = PromptArgument<Type>;
+}
+export default Prompt;
